acc5e3591a3154b214333df7094ad81d43a657fe
CurtCalledBurt <CurtCalledBurt@users.noreply.github.com>
2019-11-13 12:11:26 -0800

    added predictive capablility, added the ability to enter users on the site locally, added the ability to compare users to eachother

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -1,9 +1,13 @@
 from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User
+from .twitter import add_or_update_user
+from .predict import predict_user
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # now we make a app factory
-
 
 def create_app():
     app = Flask(__name__)
@@ -26,5 +30,51 @@ def create_app():
     def reset():
         DB.drop_all()
         DB.create_all()
-        return render_template('base_html', title='Reset', users=[])
+        return render_template('base_html',
+                               title='Reset',
+                               users=[])
+
+    # route to add users or get users
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None, message=' '):
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = f"User {name} successfully added!"
+            tweets = User.query.filter(User.name == name).one().tweets
+
+        except Exception as e:
+            message = f"Error adding {name}: {e}"
+            tweets = []
+
+        return render_template('user.html',
+                               title=name,
+                               tweets=tweets,
+                               message=message)
+
+    # adding a route for prediction
+    @app.route('/compare', methods=['POST'])
+    def compare(message=' '):
+        user1, user2 = sorted([request.values['user1'],
+                               request.values['user2']])
+        if user1 == user2:
+            message = """ What point is there comparing a user to
+                        themselves, you silly person?"""
+        else:
+            prediction = predict_user(user1,
+                                      user2,
+                                      request.values['tweet_text'])
+
+            text = request.values['tweet_text']
+            user_winner = user1 if prediction else user2
+            user_loser = user2 if prediction else user1
+
+            message = f""""{text}" is more likely to be said by
+                        {user_winner} than {user_loser}"""
+        return render_template('prediction.html',
+                               title='Prediction',
+                               message=message)
+
     return app
--- TWITOFF/twitter.py
+++ TWITOFF/twitter.py
@@ -17,6 +17,39 @@ BASILICA = basilica.Connection(config('BASILICA_KEY'))
 
 
 # additional functions
+def add_or_update_user(username):
+    """ Add or update a user and their tweets, else error """
+    try:
+        twitter_user = TWITTER.get_user(username)
+        db_user = (
+            User.query.get(twitter_user.id) or
+            User(id=twitter_user.id, name=username)
+            )
+        DB.session.add(db_user)
+        tweets = twitter_user.timeline(count=200,
+                                       exclude_replies=True,
+                                       include_rts=False,
+                                       tweet_mode='extended',
+                                       since_id=db_user.newest_tweet_id)
+        if tweets:
+            db_user.newest_tweet_id = tweets[0].id
+        for tweet in tweets:
+            # calculate embedding on the full tweet
+            embedding = BASILICA.embed_sentence(tweet.full_text,
+                                                model='twitter')
+            db_tweet = Tweet(id=tweet.id,
+                             text=tweet.full_text[:300],
+                             embedding=embedding)
+            db_user.tweets.append(db_tweet)
+            DB.session.add(db_tweet)
+    except Exception as e:
+        print(f'Error processing {username}: {e}')
+        raise e
+    else:
+        DB.session.commit()
+    # return None
+
+
 def delete_create_new_db():
     DB.drop_all()
     DB.create_all()
